2eae63d318f4199f53b1901f72f8ea3b5b8a9278
Gustavo Bernardo da Silva <gbsufscar@hotmail.com>
2024-03-08 07:55:35 -0300

    Estrutura básica do código.

---
+++ app/src/main/java/br/com/fiap/consultacep/MainActivity.kt
@@ -0,0 +1,187 @@
+package br.com.fiap.consultacep
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.com.fiap.consultacep.ui.theme.ConsultaCEPTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ConsultaCEPTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    CepScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CepScreen() {
+
+    var cepState by remember { mutableStateOf("") }
+    var ufState by remember { mutableStateOf("") }
+    var cidadeState by remember { mutableStateOf("") }
+    var ruaState by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        Card(modifier = Modifier.fillMaxWidth()) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                Text(text = "CONSULTA CEP", fontSize = 24.sp)
+                Text(
+                    text = "Encontre o seu endereço",
+                    fontSize = 20.sp
+                )
+                Spacer(modifier = Modifier.height(32.dp))
+                OutlinedTextField(
+                    value = cepState,
+                    onValueChange = {
+                        cepState = it
+                    },
+                    modifier = Modifier.fillMaxWidth(),
+                    label = {
+                        Text(text = "Qual o CEP procurado?")
+                    },
+                    trailingIcon = {
+                        IconButton(onClick = { /*TODO*/ }) {
+                            Icon(
+                                imageVector = Icons.Default.Search,
+                                contentDescription = ""
+                            )
+                        }
+                    },
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Number
+                    )
+                )
+                Spacer(modifier = Modifier.height(32.dp))
+                Text(
+                    text = "Não sabe o CEP?",
+                    fontWeight = FontWeight.Bold
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Row() {
+                    OutlinedTextField(
+                        value = ufState,
+                        onValueChange = {
+                            ufState = it
+                        },
+                        modifier = Modifier
+                            .weight(1f)
+                            .padding(end = 4.dp),
+                        label = {
+                            Text(text = "UF?")
+                        },
+                        keyboardOptions = KeyboardOptions(
+                            capitalization = KeyboardCapitalization.Characters,
+                            keyboardType = KeyboardType.Text
+                        )
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    OutlinedTextField(
+                        value = cidadeState,
+                        onValueChange = {
+                            cidadeState = it
+                        },
+                        modifier = Modifier.weight(2f),
+                        label = {
+                            Text(text = "Qual a cidade?")
+                        },
+                        keyboardOptions = KeyboardOptions(
+                            keyboardType = KeyboardType.Text,
+                            capitalization = KeyboardCapitalization.Words
+                        )
+                    )
+                }
+                Spacer(modifier = Modifier.height(8.dp))
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    OutlinedTextField(
+                        value = ruaState,
+                        onValueChange = {
+                            ruaState = it
+                        },
+                        modifier = Modifier.weight(2f),
+                        label = {
+                            Text(text = "O que lembra do nome da rua?")
+                        },
+                        keyboardOptions = KeyboardOptions(
+                            keyboardType = KeyboardType.Text,
+                            capitalization = KeyboardCapitalization.Words
+                        )
+                    )
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(imageVector = Icons.Default.Search, contentDescription = "")
+                    }
+                }
+            }
+        }
+        Spacer(modifier = Modifier.height(8.dp))
+        LazyColumn() {
+            items(120) {
+                CardEndereco()
+            }
+        }
+    }
+}
+
+@Composable
+fun CardEndereco() {
+    Card(modifier = Modifier
+        .fillMaxWidth()
+        .padding(bottom = 4.dp)) {
+        Column(modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+        ) {
+            Text(text = "CEP:")
+            Text(text = "Rua:")
+            Text(text = "Cidade:")
+            Text(text = "Bairro:")
+            Text(text = "UF:")
+        }
+    }
+}
+
